066df72b594d4dc85ac621a877bf5c238e1f86e4
Terrence Ng'andu <nganduterrence0@gmail.com>
2025-11-13 23:12:04 +0200

    Update chatHandlers.js

--- server/socket/chatHandlers.js
+++ server/socket/chatHandlers.js
@@ -1,49 +1,18 @@
-const messages = require('../models/messageModel');
-const { formatMessage } = require('../utils/helpers');
+const { Server } = require('socket.io');
+const chatHandlers = require('../controllers/chatHandlers');
 
-const users = {};
-const typingUsers = {};
-
-module.exports = (io, socket) => {
-  console.log(`User connected: ${socket.id}`);
-
-  socket.on('user_join', (username) => {
-    users[socket.id] = { username, id: socket.id };
-    io.emit('user_list', Object.values(users));
-    io.emit('user_joined', { username, id: socket.id });
-    console.log(`${username} joined the chat`);
+const initSocket = (server, CLIENT_URL) => {
+  const io = new Server(server, {
+    cors: {
+      origin: CLIENT_URL,
+      methods: ['GET', 'POST'],
+      credentials: true,
+    },
   });
 
-  socket.on('send_message', ({ message }) => {
-    const msg = formatMessage({ sender: users[socket.id]?.username || 'Anonymous', message });
-    messages.push(msg);
-    if (messages.length > 100) messages.shift();
-    io.emit('receive_message', msg);
-  });
+  io.on('connection', (socket) => chatHandlers(io, socket));
 
-  socket.on('private_message', ({ to, message }) => {
-    const msg = formatMessage({ sender: users[socket.id]?.username || 'Anonymous', message, isPrivate: true });
-    socket.to(to).emit('private_message', msg);
-    socket.emit('private_message', msg);
-  });
-
-  socket.on('typing', (isTyping) => {
-    if (!users[socket.id]) return;
-    const username = users[socket.id].username;
-    if (isTyping) typingUsers[socket.id] = username;
-    else delete typingUsers[socket.id];
-    io.emit('typing_users', Object.values(typingUsers));
-  });
-
-  socket.on('disconnect', () => {
-    if (users[socket.id]) {
-      const { username } = users[socket.id];
-      io.emit('user_left', { username, id: socket.id });
-      console.log(`${username} left the chat`);
-      delete users[socket.id];
-      delete typingUsers[socket.id];
-      io.emit('user_list', Object.values(users));
-      io.emit('typing_users', Object.values(typingUsers));
-    }
-  });
+  return io;
 };
+
+module.exports = initSocket;
